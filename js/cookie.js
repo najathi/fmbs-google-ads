d356dbdbea039aa56aea2b4e0a0caa32089ce88b
Najathi <najathi777@gmail.com>
2021-02-07 15:17:58 +0530

    pp link, cookies

---
+++ js/cookie.js
@@ -0,0 +1,11 @@
+glowCookies.start('en', {
+    // analytics: 'G-XXX',
+    facebookPixel: '869986823372823',
+    // HotjarTrackingCode: 'xxx',
+    // customScript: [{ type: 'src', position: 'head', content: 'https://www.googletagmanager.com/gtag/js?id=G-FH87DE17XF' }],
+    policyLink: 'https://fixmybrokenscreen.co.uk/privacy-policy',
+    hideAfterClick: true,
+    position: 'right',
+    bannerLinkText: 'Privacy Policy',
+    bannerDescription: "Your experience on this site will be improved by allowing cookies. ",
+});
